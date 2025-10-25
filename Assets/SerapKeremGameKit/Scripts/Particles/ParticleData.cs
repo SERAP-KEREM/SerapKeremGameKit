10b66484efb4032ee58cf0e3f9467ee6f6eba44a
Serap Kerem <66235306+SERAP-KEREM@users.noreply.github.com>
2025-10-25 21:25:27 +0300

    Update ParticleData.cs

--- Assets/SerapKeremGameKit/Scripts/Particles/ParticleData.cs
+++ Assets/SerapKeremGameKit/Scripts/Particles/ParticleData.cs
@@ -6,9 +6,13 @@ namespace SerapKeremGameKit._Particles
     [Serializable]
     public sealed class ParticleData
     {
-        public string Key;
-        public ParticleSystem Prefab;
-        public float Duration = 1f;
+        [SerializeField] private string _key;
+        [SerializeField] private ParticleSystem _prefab;
+        [SerializeField] private float _duration = 1f;
+
+        public string Key { get => _key; set => _key = value; }
+        public ParticleSystem Prefab { get => _prefab; set => _prefab = value; }
+        public float Duration { get => _duration; set => _duration = value; }
     }
 }
 
